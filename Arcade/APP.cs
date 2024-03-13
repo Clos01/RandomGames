ecebe3ab14ee64fce80dd85f3dfc06dc5767747d
Carlos Rivas <Cjrivas00@gmail.com>
2024-03-13 10:56:03 -0400

    added folders for interfaces and the game folders with the cs file in them

---
+++ Arcade/APP.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+using System.Security.Cryptography.X509Certificates;
+using AgeNameSpace;
+
+namespace App
+{
+
+    public class EntryToAPP
+    {
+
+        static void Main(string[] args)
+        {
+
+            AgeVerifier checkForLegalAge = new AgeVerifier();
+
+            Console.WriteLine("Hello, What is your name?");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"Whats up {name}, let me see your ID. (Type in Age.)");
+
+            if (int.TryParse(Console.ReadLine(), out int age))
+            {
+                bool isOldEnough = checkForLegalAge .VerifyOfLegalAge(age);
+                if (isOldEnough)
+                {
+                    Console.WriteLine("Welcome to the Arcade!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you are not old enough to enter the Arcade.");
+                }
+
+
+            }
+
+
+        }
+
+
+    }
+
+
+}
---
+++ Arcade/AgeVerification/AgeVerifier.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+
+
+
+namespace  AgeNameSpace
+{
+    public interface IAgeVerifier
+    {
+        bool VerifyOfLegalAge(int isOfAge);
+    }
+
+
+    public class AgeVerifier : IAgeVerifier
+    {
+        private const int LegalAge = 21;
+        public bool VerifyOfLegalAge(int isOfAge)
+        {
+            return isOfAge >= 21;
+
+
+        }
+    }
+
+}
